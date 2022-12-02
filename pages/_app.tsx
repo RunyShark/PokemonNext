d1b1c8c358210684cf057428d0ebf75404d39917
RunyShark <spartadragon117@live.com.mx>
2022-12-01 18:49:23 -0600

    STYLE(Theme) add theme dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import { NextUIProvider } from '@nextui-org/react';
+import { darktheme } from '../';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider theme={darktheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next';
+import { Button } from '@nextui-org/react';
 
 const HomePage: NextPage = () => {
   return (
     <div>
       <h1>Hello Word</h1>
+      <Button color={'error'} />
     </div>
   );
 };
